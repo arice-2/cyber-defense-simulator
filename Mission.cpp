eb0e5bf3d4985eb2357a2640a348c13d268d2358
av-cados <avariceoc@gmail.com>
2026-04-27 23:29:49 -0400

    added mission methods

--- Mission.cpp
+++ Mission.cpp
@@ -11,6 +11,22 @@ string Mission::GetName() const {
     return missionName;
 }
 
+int Mission::GetDifficulty() const {
+    return difficulty;
+}
+
+int Mission::GetRewardCredits() const {
+    return rewardCredits;
+}
+
+int Mission::GetRewardReputation() const {
+    return rewardReputation;
+}
+
+int Mission::GetEnergyCost() const {
+    return energyCost;
+}
+
 // Mutators
 void Mission::SetName(const string& name) {
     missionName = name;
@@ -18,4 +34,16 @@ void Mission::SetName(const string& name) {
 
 void Mission::SetDifficulty(int d) {
    difficulty = d; 
+}
+
+void Mission::SetRewardCredits(int rC) {
+    rewardCredits = rC;
+}
+
+void Mission::SetRewardReputation(int rR) {
+    rewardReputation = rR;
+}
+
+void Mission::SetEnergyCost(int eC) {
+    energyCost = eC;
 }
